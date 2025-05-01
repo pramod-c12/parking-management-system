9dffc6e49101491bca0c3568ae2f3fec6a2e5e7c
pramod c <pchidambara0625@gmail.com>
2025-05-01 21:13:08 +0530

    Fixed ci-cd.yml file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,22 @@
-require('dotenv').config();
 const url = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
-    url: url || 'postgresql://postgres:abc123@localhost:5432/parking_db',
+    url: url || 'postgresql://postgres:securepassword123@localhost:5432/parking_db',
     dialect: 'postgres',
     dialectOptions: url
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  },
+  test: {
+    url: process.env.DATABASE_URL || 'postgresql://postgres:securepassword123@localhost:5432/parking_test_db',
+    dialect: 'postgres',
+    dialectOptions: process.env.DATABASE_URL
       ? {
           ssl: {
             require: true,
